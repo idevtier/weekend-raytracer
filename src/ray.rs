880fa67fb7e12c0b28e094d8227b46386386b468
idevtier <idevtier@pop-os.localdomain>
2022-11-13 23:48:53 +0300

    Refactoring

--- src/ray.rs
+++ src/ray.rs
@@ -2,15 +2,15 @@ use crate::vec3::{Point3, Vec3};
 
 pub struct Ray<'a> {
     pub origin: &'a Point3,
-    pub dir: &'a Vec3,
+    pub direction: Vec3,
 }
 
 impl<'a> Ray<'a> {
-    pub fn new(origin: &'a Point3, dir: &'a Vec3) -> Self {
-        Self { origin, dir }
+    pub fn new(origin: &'a Point3, direction: Vec3) -> Self {
+        Self { origin, direction }
     }
 
-    pub fn at(&self, t: f64) -> Point3 {
-        self.origin + &(t * self.dir)
+    pub fn at(&self, t: &f64) -> Point3 {
+        self.origin + &(t * &self.direction)
     }
 }
